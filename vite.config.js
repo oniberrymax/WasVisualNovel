6c08bd33ce36db1adf7b73e5f993612e024814dc
oniberrymax <108061345+oniberrymax@users.noreply.github.com>
2026-03-15 04:01:49 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/WasVisualNovel/",
 });
-=======
-  base: "/WasVisualNovel/"
-});
->>>>>>> 61f4fd32b579ea9e3f1f0a8b65f8587d0c5b2428
